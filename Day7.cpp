7a8c0371536ae765f79cd8a5e4226b884e474624
Yogesh Kumar <55343959+Yogeshk4124@users.noreply.github.com>
2019-12-19 02:21:14 +0530

    Create Day7.cpp

---
+++ Day7.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int a;
+    cin>>a;
+    int ar[a];
+    for(int i=0;i<a;i++)
+    cin>>ar[i];
+    for(int i=a-1;i>=0;i--)
+    cout<<ar[i]<<" ";
+return 0;
+}    
